57f8ec2f7ab66acf1f587ae0cad044422270b0b7
Clayton Passos <netstart@gmail.com>
2020-03-25 12:14:21 -0300

    readme fix

--- nossomercadolivre-api/README.md
+++ nossomercadolivre-api/README.md
@@ -10,8 +10,6 @@ H2 console: http://localhost/h2-console
 
 In field JDBC URL inform this:
 ```jdbc:h2:mem:meumercadolivre;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE```
-
 ---
 
-
-Trello: https://trello.com/c/v7f1OUlW/14-cadastro-de-categorias
+https://trello.com/b/ntE2tuml/nosso-mercado-livre
--- nossomercadolivre-api/src/main/java/com/github/nossomercadolivre/security/UserAuthenticationController.java
+++ nossomercadolivre-api/src/main/java/com/github/nossomercadolivre/security/UserAuthenticationController.java
@@ -12,8 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.print.attribute.standard.Media;
-
 import static org.springframework.http.ResponseEntity.badRequest;
 import static org.springframework.http.ResponseEntity.ok;
 
